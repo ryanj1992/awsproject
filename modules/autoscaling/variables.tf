e8ec0c7c5e8a19d90dc7064d73082fab9cfbc5f8
ryanj <ryanj1992@hotmail.co.uk>
2021-02-12 17:38:00 +0000

    working version

--- modules/autoscaling/variables.tf
+++ modules/autoscaling/variables.tf
@@ -1,2 +1,7 @@
 variable "ecs_cluster" {}
 variable "ecs_service" {}
+variable "min_capacity" {}
+variable "max_capacity" {}
+variable "target_value" {}
+variable "scale_in_cooldown" {}
+variable "scale_out_cooldown" {}
