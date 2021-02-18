faa4e3326bc32da3073675a206ec3da9e2b8cc6c
Ryan Johnson <ryan.johnson@wrenkitchens.com>
2021-02-18 15:27:16 +0000

    ecr

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,4 @@
+# Create ECR and Endpoints only through console.
+# Create IAM role to attach to task definition
+# Then add endpoint to task definition within code
+# Then apply / hopefully this uses ECR instead of pulling image from docker hub
